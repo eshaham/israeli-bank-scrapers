c357de17f0745460ec9a99eb0ff89bbd89c4fea4
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2017-08-08 08:23:39 +0300

    unbreak some export mechanics

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import DiscountScraper from './lib/scrapers/discount';
 import LeumiCardScraper from './lib/scrapers/leumi-card';
 
-const discountScraper = new DiscountScraper().scrape;
-const leumiCardScraper = new LeumiCardScraper().scrape;
+function discountScraper(credentials, options) {
+  const scraper = new DiscountScraper();
+  return scraper.scrape(credentials, options);
+}
+
+function leumiCardScraper(credentials, options) {
+  const scraper = new LeumiCardScraper();
+  return scraper.scrape(credentials, options);
+}
 
 export { discountScraper, leumiCardScraper };
