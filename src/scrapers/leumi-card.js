a94099e970e28d96dca7ad665b380854d0dc5d0a
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2017-12-18 08:17:35 +0200

    make leumi cards amounts negative

--- src/scrapers/leumi-card.js
+++ src/scrapers/leumi-card.js
@@ -92,7 +92,7 @@ async function fetchTransactionsByType(page, accountIndex, transactionsType, sta
     return {
       date: moment(txn.dateStr, DATE_FORMAT).toDate(),
       processedDate: moment(txn.processedDateStr, DATE_FORMAT).toDate(),
-      amount: parseFloat(txn.amountStr.replace(',', '')),
+      amount: -parseFloat(txn.amountStr.replace(',', '')),
       description: txn.description.trim(),
     };
   });
