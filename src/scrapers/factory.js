03cedd12950828a057d9678293d236870ae09bca
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2018-02-04 12:42:28 +0200

    move hapoalim up in the order

--- src/scrapers/factory.js
+++ src/scrapers/factory.js
@@ -1,12 +1,14 @@
+import HapoalimScraper from './hapoalim';
 import DiscountScraper from './discount';
 import LeumiCardScraper from './leumi-card';
 import VisaCalScraper from './visa-cal';
 import IsracardScraper from './isracard';
 import AmexScraper from './amex';
-import HapoalimScraper from './hapoalim';
 
 export default function createScraper(options) {
   switch (options.companyId) {
+    case 'hapoalim':
+      return new HapoalimScraper(options);
     case 'discount':
       return new DiscountScraper(options);
     case 'visaCal':
@@ -17,8 +19,6 @@ export default function createScraper(options) {
       return new IsracardScraper(options);
     case 'amex':
       return new AmexScraper(options);
-    case 'hapoalim':
-      return new HapoalimScraper(options);
     default:
       throw new Error(`unknown company id ${options.companyId}`);
   }
