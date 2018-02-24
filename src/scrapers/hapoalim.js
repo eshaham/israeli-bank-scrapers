487603fe5be8096bca97ee6f5edcc36c35cd2a7e
Chilik Laub <chilikla@gmail.com>
2018-02-24 23:27:36 +0200

    Moving out dates of the loop and rerrange transactions fetching

--- src/scrapers/hapoalim.js
+++ src/scrapers/hapoalim.js
@@ -29,26 +29,26 @@ async function fetchAccountData(page, options) {
   const accountDataUrl = `${apiSiteUrl}/general/accounts`;
   const accountsInfo = await fetchGetWithinPage(page, accountDataUrl);
 
+  const defaultStartMoment = moment().subtract(1, 'years').add(1, 'day');
+  const startDate = options.startDate || defaultStartMoment.toDate();
+  const startMoment = moment.max(defaultStartMoment, moment(startDate));
+
+  const startDateStr = startMoment.format(DATE_FORMAT);
+  const endDateStr = moment().format(DATE_FORMAT);
+
   const accounts = [];
   for (let accountIndex = 0; accountIndex < accountsInfo.length; accountIndex += 1) {
     const accountNumber = `${accountsInfo[accountIndex].bankNumber}-${accountsInfo[accountIndex].branchNumber}-${accountsInfo[accountIndex].accountNumber}`;
 
-    const defaultStartMoment = moment().subtract(1, 'years').add(1, 'day');
-    const startDate = options.startDate || defaultStartMoment.toDate();
-    const startMoment = moment.max(defaultStartMoment, moment(startDate));
-
-    const startDateStr = startMoment.format(DATE_FORMAT);
-    const endDateStr = moment().format(DATE_FORMAT);
     const txnsUrl = `${apiSiteUrl}/current-account/transactions?accountId=${accountNumber}&numItemsPerPage=150&retrievalEndDate=${endDateStr}&retrievalStartDate=${startDateStr}&sortCode=1`;
 
     let txns;
-    await fetchGetWithinPage(page, txnsUrl)
-      .then((txnsResult) => {
-        txns = convertTransactions(txnsResult.transactions);
-      })
-      .catch(() => {
-        txns = [];
-      });
+    try {
+      const txnsResult = await fetchGetWithinPage(page, txnsUrl);
+      txns = convertTransactions(txnsResult.transactions);
+    } catch (err) {
+      txns = [];
+    }
 
     accounts.push({
       accountNumber,
