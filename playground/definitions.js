b700acf85004a838924f1beadb1c4a913dd702c6
Eran Sakal <eran@sakalim.com>
2018-04-14 16:09:29 +0300

    add playground environment, inspired heavily from israeli-ynab-updater

---
+++ playground/definitions.js
@@ -0,0 +1,5 @@
+import os from 'os';
+
+export const CONFIG_FOLDER = `${os.homedir()}/.israeli-bank-scrapers`;
+export const SETTINGS_FILE = `${CONFIG_FOLDER}/settings.json`;
+export const DOWNLOAD_FOLDER = `${os.homedir()}/Downloads/Transactions/Playground`;
---
+++ playground/setup-options.js
@@ -0,0 +1,67 @@
+import moment from 'moment';
+import inquirer from 'inquirer';
+import { SCRAPERS } from '../src';
+import { readSettingsFile, writeSettingsFile } from './helpers/settings';
+
+export default async function () {
+  const settings = await readSettingsFile();
+  const {
+    scraperId,
+    combineInstallments,
+    startDate,
+    saveLocation,
+  } = settings;
+
+  const startOfMonthMoment = moment().startOf('month');
+  const monthOptions = [];
+  for (let i = 0; i < 6; i += 1) {
+    const monthMoment = startOfMonthMoment.clone().subtract(i, 'month');
+    monthOptions.push({
+      name: monthMoment.format('ll'),
+      value: monthMoment,
+    });
+  }
+  const result = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'scraperId',
+      message: 'Which bank would you like to scrape?',
+      choices: Object.keys(SCRAPERS).map((id) => {
+        return {
+          name: SCRAPERS[id].name,
+          value: id,
+        };
+      }),
+      default: scraperId,
+
+    },
+    {
+      type: 'confirm',
+      name: 'combineInstallments',
+      message: 'Combine installment transactions?',
+      default: !!combineInstallments,
+    },
+    {
+      type: 'list',
+      name: 'startDate',
+      message: 'What date would you like to start scraping from?',
+      choices: monthOptions,
+      default: startDate,
+    },
+    {
+      type: 'input',
+      name: 'saveLocation',
+      message: 'Save folder?',
+      default: saveLocation,
+    },
+  ]);
+
+  settings.scraperId = result.scraperId;
+  settings.combineInstallments = result.combineInstallments;
+  settings.startDate = result.startDate;
+  settings.saveLocation = result.saveLocation;
+  await writeSettingsFile(settings);
+
+  console.log('playground options saved, to start scraping run "npm run playground"');
+}
+
