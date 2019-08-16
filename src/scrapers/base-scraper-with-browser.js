4d6ac754ed37290e73169895f23fcf9f6d9f9ef1
Baruch Rothkoff <baruchiro@gmail.com>
2019-08-16 10:44:36 +0300

    Check if the url is updated in navigateTo

--- src/scrapers/base-scraper-with-browser.js
+++ src/scrapers/base-scraper-with-browser.js
@@ -94,7 +94,10 @@ class BaseScraperWithBrowser extends BaseScraper {
   async navigateTo(url, page) {
     const pageToUse = page || this.page;
     const response = await pageToUse.goto(url);
-    if (!response || response.status() !== OK_STATUS) {
+
+    const responseSuccess = response && response.status() === OK_STATUS;
+    const navigated = responseSuccess || pageToUse.url() === url;
+    if (!navigated) {
       throw new Error(`Error while trying to navigate to url ${url}`);
     }
   }
