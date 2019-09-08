694759320de7bfa835f2c89014ae17068a470793
Baruch Rothkoff <baruchiro@gmail.com>
2019-09-08 20:10:21 +0300

    Support different main pages

--- src/scrapers/mizrahi.js
+++ src/scrapers/mizrahi.js
@@ -7,10 +7,12 @@ import {
 } from '../constants';
 import { BaseScraperWithBrowser, LOGIN_RESULT } from './base-scraper-with-browser';
 import { fetchPostWithinPage } from '../helpers/fetch';
+import { waitForNavigation } from '../helpers/navigation';
 
 const BASE_URL = 'https://www.mizrahi-tefahot.co.il';
 const LOGIN_URL = `${BASE_URL}/he/bank/Pages/Default.aspx`;
-const AFTER_LOGIN_BASE_URL = 'https://mto.mizrahi-tefahot.co.il/ngOnline/index.html#/main/uis/osh/p428/';
+const AFTER_LOGIN_BASE_URL = /https:\/\/mto\.mizrahi-tefahot\.co\.il\/ngOnline\/index\.html#\/main\/uis/;
+const OSH_PAGE = 'https://mto.mizrahi-tefahot.co.il/ngOnline/index.html#/main/uis/osh/p428/';
 const TRANSACTIONS_REQUEST_URL = 'https://mto.mizrahi-tefahot.co.il/Online/api/SkyOSH/get428Index';
 const DATE_FORMAT = 'DD/MM/YYYY';
 
@@ -23,7 +25,7 @@ function createLoginFields(credentials) {
 
 function getPossibleLoginResults() {
   const urls = {};
-  urls[LOGIN_RESULT.SUCCESS] = [`${AFTER_LOGIN_BASE_URL}`];
+  urls[LOGIN_RESULT.SUCCESS] = [AFTER_LOGIN_BASE_URL];
   urls[LOGIN_RESULT.INVALID_PASSWORD] = [`${BASE_URL}/login/loginMTO.aspx`];
   urls[LOGIN_RESULT.CHANGE_PASSWORD] = [
     `${AFTER_LOGIN_BASE_URL}/main/uis/ge/changePassword/`,
@@ -76,14 +78,18 @@ function convertTransactions(txns) {
 }
 
 class MizrahiScraper extends BaseScraperWithBrowser {
+  async sniffOshRequest() {
+    await waitForNavigation(this.page);
+    this.navigateTo(OSH_PAGE, this.page);
+    this.request = await this.page.waitForRequest(TRANSACTIONS_REQUEST_URL);
+  }
+
   getLoginOptions(credentials) {
     return {
       loginUrl: `${LOGIN_URL}`,
       fields: createLoginFields(credentials),
       submitButtonSelector: '#ctl00_PlaceHolderLogin_ctl00_Enter',
-      postAction: async () => {
-        this.request = await this.page.waitForRequest(TRANSACTIONS_REQUEST_URL);
-      },
+      postAction: async () => this.sniffOshRequest(),
       possibleResults: getPossibleLoginResults(),
     };
   }
