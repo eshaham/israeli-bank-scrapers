6a2ee92f973b90d3a53b95c70e9a31bc0a6dcfd8
Eran Sakal <eran@sakalim.com>
2019-10-05 17:35:29 +0300

    expose adapters from index under adapters namespace

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import puppeteerConfig from '../puppeteer-config';
+import * as adapters from './adapters/public-api';
 
 export { default as createScraper } from './scrapers/factory';
 export { SCRAPERS } from './definitions';
@@ -6,3 +7,5 @@ export { SCRAPERS } from './definitions';
 export function getPuppeteerConfig() {
   return { ...puppeteerConfig };
 }
+
+export { adapters };
