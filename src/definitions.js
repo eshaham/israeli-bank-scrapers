6a84474028665eadf5199bb7cdc2946327709973
aohana <adi_ohana@intuit.com>
2020-01-13 18:24:56 +0200

    keep same name

--- src/definitions.js
+++ src/definitions.js
@@ -26,7 +26,7 @@ export const SCRAPERS = {
     loginFields: ['username', PASSWORD_FIELD],
   },
   leumiCard: {
-    name: 'leumi Card',
+    name: 'Leumi Card',
     loginFields: ['username', PASSWORD_FIELD],
   },
   max: {
