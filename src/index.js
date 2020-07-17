b1caedbe1fe51932e8e6edfabf83a518e4288375
Eran Sakal <eran@sakalim.com>
2020-07-18 00:11:15 +0300

    extract chromium revision

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+import puppeteerConfig from '../puppeteer-config.json';
+
 export { default as createScraper } from './scrapers/factory';
 export { SCRAPERS } from './definitions';
-import { PUPPETEER_REVISIONS } from 'puppeteer';
 
 export function getPuppeteerConfig() {
-  return {
-    chromiumRevision: PUPPETEER_REVISIONS.chromium
-  };
+  return { ...puppeteerConfig };
 }
---
+++ utils/update-puppeteer-config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const checkIfCoreVariation = require('./core-utils');
+
+function getPuppeteerChromiumVersion() {
+  const puppeteerLibrary = checkIfCoreVariation() ? 'puppeteer-core' : 'puppeteer';
+  const puppeteerPath = path.dirname(require.resolve(puppeteerLibrary));
+  const revisionFilePath = path.join(puppeteerPath, 'lib/cjs/revisions.js');
+  // eslint-disable-next-line import/no-dynamic-require,global-require
+  const revisionRaw = fs.readFileSync(revisionFilePath, 'utf-8');
+  const [, revisionNumber] = revisionRaw.match(/chromium: ['"`](.+?)['"`][,]/);
+  return revisionNumber;
+}
+
+(function updatePuppeteerConfiguration() {
+  console.log('extract puppeteer chromium version from module \'puppeteer|pupetter-core\'');
+
+  const chromiumRevision = getPuppeteerChromiumVersion();
+  const configPath = path.join(__dirname, '../puppeteer-config.json');
+  // eslint-disable-next-line global-require,import/no-dynamic-require
+  const configJson = require(configPath);
+  configJson.chromiumRevision = chromiumRevision;
+
+  fs.writeFileSync(configPath, JSON.stringify(configJson, null, '  '));
+
+  console.log(`update 'src/puppeteer-config.json' file with puppeteer chroumium revision '${chromiumRevision}'`);
+}());
