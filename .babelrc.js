474d1c42d5ce6171da7977eeb7afa7c53a1d7004
Meir Asulin <mmeir12@gmail.com>
2022-02-01 16:56:10 +0200

    fix(POB-827): add as lib

--- .babelrc.js
+++ .babelrc.js
@@ -9,11 +9,13 @@ const presets = [
       corejs: "3",
     },
   ],
+  "@babel/preset-react",
   "@babel/preset-typescript",
 ];
 
 module.exports = {
   presets,
-  ignore: process.env.BABEL_ENV === "test" ? [] : ["**/*.test.(js,ts)", "tests/**/*", "src/tests/**/*"],
+  // ignore: process.env.BABEL_ENV === "test" ? [] : ["**/*.test.(js,ts)", "tests/**/*", "src/tests/**/*"],
+  ignore: ["**/*.test.(js,ts)", "tests/**/*", "src/tests/**/*"],
   plugins: ["@babel/plugin-proposal-class-properties"],
 };
