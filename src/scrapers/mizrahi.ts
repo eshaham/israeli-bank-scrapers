2c2e77618379bae7670e18a281dd7331d936685f
Naor Peled <thehecticbyte@gmail.com>
2022-03-19 04:16:28 +0200

    added english page support

--- src/scrapers/mizrahi.ts
+++ src/scrapers/mizrahi.ts
@@ -56,7 +56,9 @@ const afterLoginSelector = '#stickyHeaderScrollRegion';
 const loginSpinnerSelector = 'div.ngx-overlay.loading-foreground';
 const accountDropDownItemSelector = '#sky-account-combo-list ul li .sky-acc-value';
 const pendingTrxIdentifierId = '#ctl00_ContentPlaceHolder2_panel1';
-const emailUpdatePageTitle = "עדכון כתובת דוא''ל";
+const emailUpdatePageHebrewTitle = "עדכון כתובת דוא''ל";
+const emailUpdatePageEnglishTitle = 'Update email address';
+
 
 function createLoginFields(credentials: ScraperCredentials) {
   return [
@@ -67,7 +69,7 @@ function createLoginFields(credentials: ScraperCredentials) {
 
 function getPossibleLoginResults(page: Page): PossibleLoginResults {
   return {
-    [LoginResults.Success]: [AFTER_LOGIN_BASE_URL, async () => !!(await page.$x(`//title[contains(., "${emailUpdatePageTitle}")`))],
+    [LoginResults.Success]: [AFTER_LOGIN_BASE_URL, async () => !!(await page.$x(`//title[contains(., "${emailUpdatePageHebrewTitle}") or contains(., "${emailUpdatePageEnglishTitle}")]`))],
     [LoginResults.InvalidPassword]: [async () => !!(await page.$(invalidPasswordSelector))],
     [LoginResults.ChangePassword]: [CHANGE_PASSWORD_URL],
   };
