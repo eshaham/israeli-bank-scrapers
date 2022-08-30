f1f6a178c03086b16e15d183024a7755599386fb
Baruch Odem (Rothkoff) <baruchiro@gmail.com>
2022-08-31 00:04:31 +0300

    fix: Mizrahi scraping issues when user does not need to update the email (#710)

--- src/scrapers/mizrahi.ts
+++ src/scrapers/mizrahi.ts
@@ -6,6 +6,7 @@ import {
 } from '../helpers/elements-interactions';
 import { fetchPostWithinPage } from '../helpers/fetch';
 import { waitForUrl } from '../helpers/navigation';
+import { raceTimeout, SECOND } from '../helpers/waiting';
 import {
   Transaction, TransactionsAccount, TransactionStatuses, TransactionTypes,
 } from '../transactions';
@@ -192,7 +193,7 @@ class MizrahiScraper extends BaseScraperWithBrowser {
 
   private async fetchAccount() {
     // workaround for situations where Mizrahi pops up pages (e.g. email update page)
-    await this.page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await raceTimeout(5 * SECOND, this.page.waitForNavigation({ waitUntil: 'networkidle2' }));
     await this.page.$eval(`a[href="${OSH_PAGE}"]`, (el) => (el as HTMLElement).click());
     await waitUntilElementFound(this.page, `a[href="${TRANSACTIONS_PAGE}"]`);
     await this.page.$eval(`a[href="${TRANSACTIONS_PAGE}"]`, (el) => (el as HTMLElement).click());
