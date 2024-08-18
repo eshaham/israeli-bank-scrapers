4a3976b6187b137c89637949ebf121a9064832d6
Avraham wales <avraham.wales@wiahtrip.com>
2024-08-18 13:10:54 +0300

    Add bank pagi scraper

---
+++ src/scrapers/pagi.ts
@@ -0,0 +1,12 @@
+import BeinleumiGroupBaseScraper from './base-beinleumi-group';
+
+class PagiScraper extends BeinleumiGroupBaseScraper {
+  BASE_URL = 'https://online.pagi.co.il/';
+
+  LOGIN_URL = `${this.BASE_URL}/MatafLoginService/MatafLoginServlet?bankId=PAGIPORTAL&site=Private&KODSAFA=HE`;
+
+  TRANSACTIONS_URL = `${this.BASE_URL}/wps/myportal/FibiMenu/Online/OnAccountMngment/OnBalanceTrans/PrivateAccountFlow`;
+
+}
+
+export default PagiScraper;
