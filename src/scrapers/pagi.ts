215f57a34a1258df9e63709e1d015813634b1f8b
Y-Ploni <7353755@gmail.com>
2025-04-18 13:57:15 +0300

    הוספת בנק פאגי

--- src/scrapers/pagi.ts
+++ src/scrapers/pagi.ts
@@ -1,12 +1,11 @@
 import BeinleumiGroupBaseScraper from './base-beinleumi-group';
 
-class PagiScraper extends BeinleumiGroupBaseScraper {
-  BASE_URL = 'https://online.pagi.co.il/';
+class BeinleumiScraper extends BeinleumiGroupBaseScraper {
+  BASE_URL = 'https://online.pagi.co.il';
 
-  LOGIN_URL = `${this.BASE_URL}/MatafLoginService/MatafLoginServlet?bankId=PAGIPORTAL&site=Private&KODSAFA=HE`;
+  LOGIN_URL = `${this.BASE_URL}/MatafLoginService/MatafLoginServlet?bankId=FIBIPORTAL&site=Private&KODSAFA=HE`;
 
   TRANSACTIONS_URL = `${this.BASE_URL}/wps/myportal/FibiMenu/Online/OnAccountMngment/OnBalanceTrans/PrivateAccountFlow`;
-
 }
 
-export default PagiScraper;
+export default BeinleumiScraper;
