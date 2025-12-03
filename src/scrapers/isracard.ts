fdb7dc47a70728c46b1141ca315fb7e81de3a80b
Daniel Hauser <dd.hauser@gmail.com>
2025-12-03 13:13:49 +0200

    refactor(isracard): remove debug import and enable

--- src/scrapers/isracard.ts
+++ src/scrapers/isracard.ts
@@ -1,11 +1,8 @@
-import debug from 'debug';
 import IsracardAmexBaseScraper from './base-isracard-amex';
 import { type ScraperOptions } from './interface';
 
 const BASE_URL = 'https://digital.isracard.co.il';
 const COMPANY_CODE = '11';
-
-debug.enable('israeli-bank-scrapers:base-isracard-amex');
 
 class IsracardScraper extends IsracardAmexBaseScraper {
   constructor(options: ScraperOptions) {
