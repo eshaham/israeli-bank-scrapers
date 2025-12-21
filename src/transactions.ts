2ad40fb2813c33250cc5e70bb9dcd9931ef7f04f
Skeli <17243618+Skeli789@users.noreply.github.com>
2025-12-21 18:09:30 +0200

    Add volume to security details

--- src/transactions.ts
+++ src/transactions.ts
@@ -57,6 +57,7 @@ export interface Transaction {
 export interface Security {
   name?: string;
   symbol: string;
+  volume: number;
   value: number;
   currency?: string;
   changePercentage?: number;
